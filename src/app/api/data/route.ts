5a34a3ef7012661d74e4e55262fc5bd272016c88
Ivan Galan <igpastor@dminc.com>
2023-05-03 20:30:01 +0200

    change api response

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,10 +1,10 @@
 import data from './pages.json';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, response: NextResponse) {
   const slug = request.nextUrl.searchParams.get('slug');
 
   const pageData = data.pages.find((page) => page.slug === slug);
 
-  return new Response(JSON.stringify(pageData));
+  return NextResponse.json(pageData);
 }
